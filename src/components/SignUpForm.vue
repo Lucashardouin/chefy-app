c314bf7f1ee77afd44ef63eec7659ef5c0ca1bcf
Lucashardouin <hardouin.lucas@gmail.com>
2023-08-25 14:24:37 +0200

    test inscription

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -1,15 +1,15 @@
 <template>
     <form @submit.prevent="submitInscription">
-      <input v-model="restaurant_name" placeholder="Nom du restaurant" />
-      <input v-model="username" placeholder="Identifiant" />
-      <input v-model="email" type="email" placeholder="Email" />
-      <input v-model="password" type="password" placeholder="Mot de passe" />
-      <input v-model="adresse" placeholder="Adresse" />
-      <input v-model="ville" placeholder="Ville" />
-      <input v-model="code_postal" placeholder="Code postal" />
-      <input ref="imageInput" type="file" @change="handleImageChange" />
-      <img :src="image" alt="Image sélectionnée" />
-      <input type="hidden" name="role" />
+      <input v-model="newUser.restaurant_name" name="restaurant_name" placeholder="Nom du restaurant" />
+      <input v-model="newUser.username" name="username" placeholder="Identifiant" />
+      <input v-model="newUser.email" name="email" type="email" placeholder="Email" />
+      <input v-model="newUser.mdp" name="password" type="password" placeholder="Mot de passe" />
+      <input v-model="newUser.adresse" name="adresse" placeholder="Adresse" />
+      <input v-model="newUser.ville" name="ville" placeholder="Ville" />
+      <input v-model="newUser.code_postal" name="code_postal" placeholder="Code postal" />
+      <input ref="imageInput" type="file" name="image" @change="handleImageChange" />
+      <img v-if="newUser.image" :src="selectedImage" alt="Image sélectionnée" />
+      <input type="hidden" name="role" v-model="newUser.role" />
       <button type="submit">S'inscrire</button>
     </form>
   </template>
@@ -20,33 +20,38 @@
   export default {
     data() {
       return {
-        restaurant_name: '',
-        username: '',
-        email: '',
-        password: '',
-        adresse: '',
-        ville: '',
-        code_postal: '',
-        image: null,
-        role: 'user' 
+        newUser: {
+          restaurant_name: '',
+          username: '',
+          email: '',
+          mdp: '',
+          adresse: '',
+          ville: '',
+          code_postal: '',
+          image: null,
+          role: 'user'
+        },
+        selectedImage: null   
       };
     },
     methods: {
       async submitInscription() {
         try {
-          // Créez un objet FormData pour envoyer les données, y compris le fichier
-          const formData = new FormData();
-          formData.append('restaurant_name', this.restaurant_name);
-          formData.append('username', this.username);
-          formData.append('email', this.email);
-          formData.append('mdp', this.mdp);
-          formData.append('adresse', this.adresse);
-          formData.append('ville', this.ville);
-          formData.append('code_postal', this.code_postal);
-          formData.append('image', this.image); // Envoyez l'image sélectionnée
-          formData.append('role', this.role);
+          console.log('restaurant_name:', this.newUser.restaurant_name);
+        const formData = new FormData();
+          formData.append('restaurant_name', this.newUser.restaurant_name);
+          formData.append('username', this.newUser.username);
+          formData.append('email', this.newUser.email);
+          formData.append('mdp', this.newUser.mdp);
+          formData.append('adresse', this.newUser.adresse);
+          formData.append('ville', this.newUser.ville);
+          formData.append('code_postal', this.newUser.code_postal);
+          formData.append('image', this.newUser.image); // Envoyez l'image sélectionnée
+          formData.append('role', this.newUser.role);
+
+          console.log('formData:', formData);
   
-          const response = await axios.post('/api/inscription', formData, {
+          const response = await axios.post('http://localhost:5001/api/users/', formData, {
             headers: {
               'Content-Type': 'multipart/form-data', // Spécifiez le type de contenu pour envoyer le fichier
             },
@@ -61,6 +66,7 @@
   
         if (file) {
           // Afficher une prévisualisation de l'image
+          this.newUser.image = file;
           this.selectedImage = URL.createObjectURL(file);
         }
       },
